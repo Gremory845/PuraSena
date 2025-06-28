9002280d9364163e944e28756d190544d6dda144
LuanaSalasA <luana.salas.a@gmail.com>
2025-06-27 21:45:25 -0600

    correccion de error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../assets/Login.css';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
